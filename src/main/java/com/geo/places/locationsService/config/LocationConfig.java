7f32d38586acea25ed04c3c4a1a42e70d9684300
Gabriel de Souza Maia. <gabrielsouzamaia99@gmail.com>
2024-08-10 23:30:53 -0300

    changed bean configuration due to errors

--- src/main/java/com/geo/places/locationsService/config/LocationConfig.java
+++ src/main/java/com/geo/places/locationsService/config/LocationConfig.java
@@ -4,7 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
 
+import com.geo.places.locationsService.domain.repository.BiomeRepository;
 import com.geo.places.locationsService.domain.repository.LocationRepository;
+import com.geo.places.locationsService.domain.service.BiomeService;
 import com.geo.places.locationsService.domain.service.LocationService;
 
 @Configuration
@@ -14,5 +16,10 @@ public class LocationConfig {
     @Bean LocationService locationService(LocationRepository locationRepository){
 
         return new LocationService(locationRepository);
+    }
+    
+    @Bean BiomeService biomeService(BiomeRepository biomeRepository){
+
+        return new BiomeService(biomeRepository);
     }
 }
